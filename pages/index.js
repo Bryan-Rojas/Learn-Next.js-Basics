f38c53c1f169f066150a93289087f9f96b96d421
Bryan Rojas <bryroj14@gmail.com>
2019-05-14 00:30:57 -0700

    Created dyanmic pages

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 import Layout from '../components/layout'
+import Link from 'next/link'
+
+const PostLink = props => (
+    <li>
+        <Link href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+)
 
 const Index = () => (
     <Layout>
-        <p>Hello Next.js</p>
+        <p>My Blog</p>
+        <ul>
+            <PostLink title="Hello Next.js" />
+            <PostLink title="Learn Next.js is awesome" />
+            <PostLink title="Deploy apps with Zeit" />
+        </ul>
     </Layout>
 )
 
