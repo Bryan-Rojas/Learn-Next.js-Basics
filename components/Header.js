75e76cb72fbfc57960832ba2a429f0f6a54f9521
Bryan Rojas <bryroj14@gmail.com>
2019-05-19 19:16:32 -0700

    fixed links

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const linkStyle = {
 
 const Header = () => (
     <div>
-        <Link href="/"><a style={linkStyle}>Homee</a></Link>
+        <Link href="/"><a style={linkStyle}>Home</a></Link>
         <Link href="/about"><a style={linkStyle}>About</a></Link>
     </div>
 )
